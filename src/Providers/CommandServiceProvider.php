95e8feecf8697e895951a436094f2bfe8acf1614
ActCMS <actcms.com@gmail.com>
2024-11-28 23:49:59 +0700

    Initial commit

---
+++ src/Providers/CommandServiceProvider.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace ACTCMS\GitCommitChecker\Providers;
+
+use ACTCMS\GitCommitChecker\Commands\InstallCommand;
+use ACTCMS\GitCommitChecker\Commands\PreCommitHookCommand;
+use Illuminate\Support\ServiceProvider;
+
+class CommandServiceProvider extends ServiceProvider
+{
+    public function boot(): void
+    {
+        if (! $this->app->runningInConsole()) {
+            return;
+        }
+
+        $this->commands([
+            InstallCommand::class,
+            PreCommitHookCommand::class,
+        ]);
+    }
+}
